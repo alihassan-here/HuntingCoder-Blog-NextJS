3440426eb65f9bd1339cbb23005805729474d2ef
alihassan-here <alihassanuog50@gmail.com>
2022-02-19 23:30:14 +0500

    global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+//we can only import global styles in in this module
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
